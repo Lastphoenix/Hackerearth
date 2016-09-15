1ed942089544748911608c24b3181e1ee1eea7b7
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-09-15 09:57:05 -0300

    Still TLE
    Still TLE in the last two

--- bitmanipulation/monkandtasks/com/Monk.java
+++ bitmanipulation/monkandtasks/com/Monk.java
@@ -1,30 +1,90 @@
 package monkandtasks.com;
 
 import java.io.*;
-import java.math.BigInteger;
+import java.math.*;
 import java.util.*;
-
-//Almost... The last Two gave TLE.
 
 public class MONK {
 
+	final static class FastReader {
+
+	    private final BufferedReader bufferedReader;
+	    private StringTokenizer tokenizer;
+
+	    private FastReader(final BufferedReader bufferedReader) {
+	        this.bufferedReader = bufferedReader;
+	        this.tokenizer = null;
+	    }
+
+	    public final static FastReader from(final InputStream inputStream) {
+	        return new FastReader(new BufferedReader(new InputStreamReader(inputStream)));
+	    }
+
+	    public String next() {
+	        return tokenize() ? tokenizer.nextToken() : null;
+	    }
+
+	    public boolean canReadMore() {
+	        return tokenize();
+	    }
+
+	    private boolean tokenize() {
+	        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+	            
+	            String line = null;
+	            try {
+	                if ((line = bufferedReader.readLine()) == null) return false;
+	            } catch (IOException unexpected) {
+	                throw new RuntimeException(unexpected);
+	            }
+	            tokenizer = new StringTokenizer(line);
+	        }
+	        return true;
+	    }
+
+	    public int nextInt() {
+	        return Integer.parseInt(next());
+	    }
+
+	    public long nextLong() {
+	        return Long.parseLong(next());
+	    }
+
+	    public double nextDouble() {
+	        return Double.parseDouble(next());
+	    }
+
+	    public BigDecimal nextBigDecimal() {
+	        return new BigDecimal(next());
+	    }
+
+	    public BigInteger nextBigInteger() {
+	        return new BigInteger(next());
+	    }
+
+	    public void close() {
+	        try {
+	            bufferedReader.close();
+	        } catch (IOException unexpected) {
+	            throw new RuntimeException(unexpected);
+	        }
+	    }
+	}
+	
 	public static void main(String[] args) throws Exception {
 
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Desenvolvedor\\Desktop\\teste.txt"));
-		int tests = Integer.parseInt(br.readLine());
+		final FastReader fr = FastReader.from(System.in);
+		int tests = fr.nextInt();
 
 		while (tests-- > 0) {
 
-			int n = Integer.parseInt(br.readLine());
-			String[] s = br.readLine().split(" ");
+			int n = fr.nextInt();
 			BigInteger[] input = new BigInteger[n];
 			Integer[] ones = new Integer[n];
 
 			for (int i = 0; i < n; i++) {
-				BigInteger x = new BigInteger(s[i]);
-				input[i] = x;
-				ones[i] = countOnesBinary(x);
+				input[i] = fr.nextBigInteger();
+				ones[i] = countOnesBinary(input[i]);
 			}
 			
 			List<BigInteger> bigInput = Arrays.asList(input);
@@ -32,13 +92,13 @@ public class MONK {
 			
 			concurrentSort(onesBinary, bigInput);
 			
-			String ans = bigInput.toString()
-			.replace(",", "")  
-		    .replace("[", "")  
-		    .replace("]", "");
-			System.out.println(ans);
+			StringBuilder stb = new StringBuilder();
+				for(BigInteger x : bigInput) {
+					stb.append(x + " ");
+			}
+			StdOut.println(stb);
 		}
-		br.close();
+		fr.close();
 	}
 
 	private static Integer countOnesBinary(BigInteger x) {
@@ -83,4 +143,44 @@ public class MONK {
 			for (int i = 0; i < list.size(); i++)
 				Collections.swap(list, swapFrom.get(i), swapTo.get(i));
 	}
+
+}
+
+class StdOut {
+
+    private static String charsetName = "UTF-8";
+
+    private static PrintWriter out;
+
+    static {
+        try {
+            out = new PrintWriter(new OutputStreamWriter(System.out, charsetName), true);
+        }
+        catch (UnsupportedEncodingException e) { System.out.println(e); }
+    }
+
+    private StdOut() { }
+
+    public static void close() { out.close(); }
+
+    public static void println()          { out.println();  }
+    public static void println(Object x)  { out.println(x); }
+    public static void println(boolean x) { out.println(x); }
+    public static void println(char x)    { out.println(x); }
+    public static void println(double x)  { out.println(x); }
+    public static void println(float x)   { out.println(x); }
+    public static void println(int x)     { out.println(x); }
+    public static void println(long x)    { out.println(x); }
+
+    public static void print(Object x)    { out.print(x);   }
+    public static void print(boolean x)   { out.print(x);   }
+    public static void print(char x)      { out.print(x);   }
+    public static void print(double x)    { out.print(x);   }
+    public static void print(float x)     { out.print(x);   }
+    public static void print(int x)       { out.print(x);   }
+    public static void print(long x)      { out.print(x);   }
+
+    public static void printf(String format, Object... args) {
+        out.printf(format, args);
+    }
 }
