6d7d8499663298bf7fc287434413ad35f43344af
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-09-19 14:49:10 -0300

    AC LUCKY
    There has to be better way to do it.

--- bitmanipulation/luckynumbers/com/LUCKY.java
+++ bitmanipulation/luckynumbers/com/LUCKY.java
@@ -1,11 +1,7 @@
 package luckynumbers.com;
 
 import java.io.*;
-
-/*  Partially Solved
- *  Sum get more than long.max_value
- *  Input 2 --> 742959321256734880 Output: 555498453
- */
+import java.math.BigInteger;
 
 public class LUCKY {
 
@@ -13,21 +9,40 @@ public class LUCKY {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		long mod = 1000000007;
 
 		while (t-- > 0) {
 
 			long n = Long.parseLong(br.readLine());
 			long sum = 0;
+			BigInteger sum1 = BigInteger.ZERO;
 
-			for (long i = 2; i < n; i *= 2) {
-				for (long j = 1; j < i; j *= 2) {
-					if (i + j <= n) {
-						sum += i + j;
+			if (n > 242959321256734880L) {
+
+				BigInteger mod = BigInteger.valueOf(1000000007L);
+				for (long i = 2; i < n; i *= 2) {
+					for (long j = 1; j < i; j *= 2) {
+						if (i + j <= n) {
+							BigInteger x = BigInteger.valueOf(i);
+							BigInteger y = BigInteger.valueOf(j);
+							BigInteger xy = x.add(y);
+							sum1 = sum1.add(xy);
+						}
 					}
-				}					
+				}
+				System.out.println(sum1.mod(mod));
+
+			} else {
+
+				long mod = 1000000007;
+				for (long i = 2; i < n; i *= 2) {
+					for (long j = 1; j < i; j *= 2) {
+						if (i + j <= n) {
+							sum += i + j;
+						}
+					}
+				}
+				System.out.println(sum % mod);
 			}
-			System.out.println(sum % mod);
 		}
 	}
 }
---
+++ bitmanipulation/microandbinarystrings/com/MICRO.java
@@ -0,0 +1,10 @@
+package microandbinarystrings.com;
+
+public class MICRO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
