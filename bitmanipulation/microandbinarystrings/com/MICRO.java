87e398370af80469785b2b613ff4bc6ec3fbe1ed
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-09-20 13:57:32 -0300

    AC MICRO
    After getting the logic the code is easy.

--- bitmanipulation/microandbinarystrings/com/MICRO.java
+++ bitmanipulation/microandbinarystrings/com/MICRO.java
@@ -1,10 +1,27 @@
 package microandbinarystrings.com;
 
+import java.io.*;
+
 public class MICRO {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+
+			int N = Integer.parseInt(br.readLine());
+			String S = br.readLine();
+			int count = 0;
+
+			for (int i = 0; i < N; i++) {
+
+				if (S.charAt(i) == '1') {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
 	}
-
 }
---
+++ bitmanipulation/xsquareandtwostrings/com/XSQUARE.java
@@ -0,0 +1,16 @@
+package xsquareandtwostrings.com;
+
+import java.io.*;
+
+public class XSQUARE {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		while (T-- > 0) {
+			
+		}
+	}
+}
