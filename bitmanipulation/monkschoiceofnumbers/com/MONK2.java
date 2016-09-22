d27a51092531b83283a1336caed4a2db8dc665d7
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-09-22 16:29:34 -0300

    AC MONK2

--- bitmanipulation/monkschoiceofnumbers/com/MONK2.java
+++ bitmanipulation/monkschoiceofnumbers/com/MONK2.java
@@ -1,6 +1,7 @@
 package monkschoiceofnumbers.com;
 
 import java.io.*;
+import java.util.*;
 
 public class MONK2 {
 
@@ -9,13 +10,25 @@ public class MONK2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 
-
 		while (t-- > 0) {
-			
+
 			String[] line = br.readLine().split(" ");
 			int n = Integer.parseInt(line[0]);
 			int k = Integer.parseInt(line[1]);
 			String[] numbers = br.readLine().split(" ");
+			Integer[] ones = new Integer[n];
+			long ans = 0;
+
+			for (int i = 0; i < n; i++) {
+				Integer x = Integer.parseInt(numbers[i]);
+				ones[i] = Integer.bitCount(x);
+			}
+			Arrays.sort(ones, Collections.reverseOrder());
+
+			for (int j = 0; j < k; j++) {
+				ans += ones[j];
+			}
+			System.out.println(ans);
 		}
 	}
 }
