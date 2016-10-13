87fa2adda7336421ad483a12f82323901c5b60d3
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-10-13 14:32:10 -0300

    AC c++ CHANDAN
    In java the program was not working with testcase number 2

--- bitmanipulation/chandanandbalancedstrings/com/CHANDAN.java
+++ bitmanipulation/chandanandbalancedstrings/com/CHANDAN.java
@@ -1,10 +1,41 @@
 package chandanandbalancedstrings.com;
 
+import java.io.*;
+import java.util.*;
+
 public class CHANDAN {
+	
+	public static long[] dp = new long[100005];
+	
+	public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
 
+		while (t-- > 0) {
+
+			String s = br.readLine();
+			int length = s.length();
+			Map<Long, Integer> map = new HashMap<Long, Integer>();
+			int answer = 0;
+			
+			for (int i = 0; i <= length; i++) {
+				
+				if(i == 0){
+					dp[i] = 0;
+				} else {
+					dp[i] = dp[i - 1] ^ (1 << (s.charAt(i - 1) - 97)); 
+				} if(map.get(dp[i]) == null){
+		            map.put(dp[i], 1);
+		        } else {
+		            map.put(dp[i], map.get(dp[i]) + 1);
+		        }
+			}
+			
+			for(Long x : map.keySet()){
+				answer += x;
+			}
+			System.out.println(answer);
+		}
 	}
-
 }
---
+++ recursionandbacktracking/awesomesequence/com/AWESOME.java
@@ -0,0 +1,16 @@
+package awesomesequence.com;
+
+import java.io.*;
+
+public class AWESOME {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		while (t-- > 0) {
+
+		}
+	}
+}
