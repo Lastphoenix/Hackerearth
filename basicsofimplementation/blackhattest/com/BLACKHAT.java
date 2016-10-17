60d62ea621bc9b94050aadeeb56c611ef856dced
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-10-17 15:34:19 -0200

    AC jhool
    AC jhool

---
+++ basicsofimplementation/blackhattest/com/BLACKHAT.java
@@ -0,0 +1,16 @@
+package blackhattest.com;
+
+import java.io.*;
+
+public class BLACKHAT {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		while (t-- > 0){
+			
+		}		
+	}
+}
--- basicsofimplementation/littlejhoolandpsychicpowers/com/JHOOL.java
+++ basicsofimplementation/littlejhoolandpsychicpowers/com/JHOOL.java
@@ -8,17 +8,8 @@ public class JHOOL {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		int countZeros = 0;
-		int countOnes = 0;
-		
-		for(int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == '0') {
-				countZeros++;
-			} else {
-				countOnes++;
-			}
-		}
-		if (countZeros == 6 || countOnes == 6) {
+
+		if (s.contains("111111") || s.contains("000000")) {
 			System.out.println("Sorry, sorry!");
 		} else {
 			System.out.println("Good luck!");
