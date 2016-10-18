c40caf11fe196d533158f25ce5352757cf29048c
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-10-18 15:02:37 -0200

    compilation error

--- basicsofimplementation/blackhattest/com/BLACKHAT.java
+++ basicsofimplementation/blackhattest/com/BLACKHAT.java
@@ -7,10 +7,39 @@ public class BLACKHAT {
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine().trim());
+
+		while (t-- > 0) {
+
+			String s = br.readLine().trim();
+			StringBuilder stb = new StringBuilder();
+			char[] c = s.toCharArray();
+			int x = 0;
+
+			for (int i = 0; i < s.length(); i++) {
+
+				int y = countLetters(c, s.charAt(i), i);
+				x = s.charAt(i) + 13 + y;
+
+				if (x > 122) {
+					stb.append(Character.toChars(x - 26));
+				} else {
+					stb.append(Character.toChars(x));
+				}
+			}
+			System.out.println(stb);
+		}
+	}
+
+	private static int countLetters(char[] c, char x, int i) {
+
+		int count = 0;
 		
-		while (t-- > 0){
-			
-		}		
+		for (int j = 0; j < i; j++) {
+			if (c[j] == x) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
