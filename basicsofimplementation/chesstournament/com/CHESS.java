46c4ffd2f7858e08469b19be64221a1df2a16761
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-11-07 09:13:13 -0200

    still in development...
    still in development...

--- basicsofimplementation/chesstournament/com/CHESS.java
+++ basicsofimplementation/chesstournament/com/CHESS.java
@@ -1,6 +1,7 @@
 package chesstournament.com;
 
 import java.io.*;
+import java.util.*;
 
 public class CHESS {
 
@@ -10,7 +11,12 @@ public class CHESS {
 		int N = Integer.parseInt(br.readLine());
 		int lines = (int) Math.pow(2, N);
 		int[][] triangularMatrix = new int[lines - 1][];
-	
+		int[] participants = new int[lines];
+		
+		for(int i = 0; i < lines; i++) {
+			participants[i] = i + 1;
+		}
+		
 		for(int i = 0; i < lines - 1; i++){
 			String[] input = br.readLine().split(" ");
 			triangularMatrix[i] = new int[i + 1];
@@ -18,12 +24,27 @@ public class CHESS {
 				triangularMatrix[i][j] = inputToInt(input, j);
 			}	
 		}	
-		System.out.println(winner(triangularMatrix));
+		System.out.println(winner(triangularMatrix, participants, lines));
 	}
 
-	private static int winner(int[][] triangularMatrix) {
+	private static int winner(int[][] triangularMatrix, int[] participants, int lines) {
 		
+		List<Integer> list = new ArrayList<Integer>();
 		int winner = 1;
+		
+		for(int i = 0; i < lines - 1; i++) {
+			for(int j = 0; j < i; j++) {
+				int value = triangularMatrix[i][j];
+				if (value == 1) {
+					int y = j + 1;
+					list.add(participants[y]);
+				} else {
+					list.add(participants[j]);
+				}
+			}
+		}
+		
+		System.out.println(list);
 		return winner;
 	}
 
