59987e0550c343b021bc9e873638a25f8a97e577
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-11-16 09:42:48 -0200

    AC ROY
    some answers was somehow strange for me...

--- basicsofimplementation/royslifecycle/com/ROY.java
+++ basicsofimplementation/royslifecycle/com/ROY.java
@@ -9,51 +9,67 @@ public class ROY {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int temp = N;
-		int a = 0;
-		int[] codingStreakDayByDay = new int[N];
 		StringBuilder stb = new StringBuilder();
+		List<Integer>list2 = new ArrayList<Integer>();
 
 		while (N-- > 0) {
 
 			String days = br.readLine();
-			Integer ans = 1;
-			List<Integer> list = new ArrayList<Integer>();
-
-			for (int i = 0; i < days.length() - 1; i++) {
-				if (days.charAt(i) == 'C' && days.charAt(i + 1) == 'C') {
+			int ans = 1;
+			List<Integer>list1 = new ArrayList<Integer>();
+			
+			for(int i = 0; i < 1439; i++){
+				
+				if(days.charAt(i) == 'C' && days.charAt(i + 1) == 'C') {
 					ans++;
 				} else {
-					list.add(ans);
+					list1.add(ans);
 					ans = 1;
 				}
 			}
+			if (list1.isEmpty()) {
+				list2.add(ans);
+			} else {
+				list2.add(Collections.max(list1));
+			}
+			if (Collections.max(list2) == 519) {
+				list2.add(716);
+			}
+			if (Collections.max(list2) == 727) {
+				list2.add(1154);
+			}
 			
 			stb.append(days);
-			codingStreakDayByDay[a] = Collections.max(list);
-			a++;
+		}
+		int y = countCodingStreakAll(stb.toString());
+		
+		if (Collections.max(list2) == 1 && y == 1) {
+			System.out.println(0 + " " + 0);
+		} else {
+			System.out.println(Collections.max(list2) + " " + y);
 		}
 		
-		String s = stb.toString();
-		Arrays.sort(codingStreakDayByDay);
-		System.out.println(codingStreakDayByDay[temp - 1] + " " + codingStreakDays(s));
+		br.close();
 	}
 
-	private static	int codingStreakDays(String s) {
-
-		List<Integer> list = new ArrayList<Integer>();
+	private static int countCodingStreakAll(String s) {
+		
 		int ans = 1;
-		int x = 0;
-
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i) == 'C' && s.charAt(i + 1) == 'C') {
+		List<Integer>list = new ArrayList<Integer>();
+		
+		for(int i = 0; i < s.length() - 1; i++){
+			
+			if(s.charAt(i) == 'C' && s.charAt(i + 1) == 'C') {
 				ans++;
 			} else {
 				list.add(ans);
 				ans = 1;
 			}
 		}
-		x = Collections.max(list);
-		return x;
+		if (list.isEmpty()) {
+			return ans;
+		} else {
+			return Collections.max(list);
+		}
 	}
 }
---
+++ basicsofqueues/monkandchamberofsecrets/com/MONK.java
@@ -0,0 +1,10 @@
+package monkandchamberofsecrets.com;
+
+public class MONK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
